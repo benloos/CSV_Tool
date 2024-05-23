c9431b84d84f737bcb0d1923964f04eb90940a64
Ben Loos <ben.loos47@live.com>
2024-05-23 14:53:11 +0200

    rename project

---
+++ CSVTool.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CSVTool
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnImportCSV = New Button()
+        lbColumns = New ListBox()
+        dgvCSV = New DataGridView()
+        btnReplaceData = New Button()
+        btnOpenLogs = New Button()
+        CType(dgvCSV, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btnImportCSV
+        ' 
+        btnImportCSV.Location = New Point(12, 12)
+        btnImportCSV.Name = "btnImportCSV"
+        btnImportCSV.Size = New Size(90, 23)
+        btnImportCSV.TabIndex = 0
+        btnImportCSV.Text = "Import CSV"
+        btnImportCSV.UseVisualStyleBackColor = True
+        ' 
+        ' lbColumns
+        ' 
+        lbColumns.FormattingEnabled = True
+        lbColumns.ItemHeight = 15
+        lbColumns.Location = New Point(633, 41)
+        lbColumns.Name = "lbColumns"
+        lbColumns.Size = New Size(155, 109)
+        lbColumns.TabIndex = 2
+        ' 
+        ' dgvCSV
+        ' 
+        dgvCSV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvCSV.Location = New Point(12, 41)
+        dgvCSV.Name = "dgvCSV"
+        dgvCSV.Size = New Size(615, 397)
+        dgvCSV.TabIndex = 3
+        ' 
+        ' btnReplaceData
+        ' 
+        btnReplaceData.Location = New Point(633, 156)
+        btnReplaceData.Name = "btnReplaceData"
+        btnReplaceData.Size = New Size(90, 23)
+        btnReplaceData.TabIndex = 4
+        btnReplaceData.Text = "Replace Data"
+        btnReplaceData.UseVisualStyleBackColor = True
+        ' 
+        ' btnOpenLogs
+        ' 
+        btnOpenLogs.Location = New Point(633, 415)
+        btnOpenLogs.Name = "btnOpenLogs"
+        btnOpenLogs.Size = New Size(90, 23)
+        btnOpenLogs.TabIndex = 5
+        btnOpenLogs.Text = "Open Logs"
+        btnOpenLogs.UseVisualStyleBackColor = True
+        ' 
+        ' CSVTool
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnOpenLogs)
+        Controls.Add(btnReplaceData)
+        Controls.Add(dgvCSV)
+        Controls.Add(lbColumns)
+        Controls.Add(btnImportCSV)
+        Name = "CSVTool"
+        Text = "CSV Tool"
+        CType(dgvCSV, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnImportCSV As Button
+    Friend WithEvents lbColumns As ListBox
+    Friend WithEvents dgvCSV As DataGridView
+    Friend WithEvents btnReplaceData As Button
+    Friend WithEvents btnOpenLogs As Button
+
+End Class
---
+++ CSVTool.vb
@@ -0,0 +1,93 @@
+﻿Imports System.Reflection.Metadata
+Imports CSV_Tool.FileHandler, CSV_Tool.Logger
+
+Public Class CSVTool
+    ''' <summary>
+    ''' Struktur:
+    '''     GUI:
+    '''         TAB CSV
+    '''         - Datei laden mit csv
+    '''         CSV Viewer
+    '''         - Spaltenauswahl (dropdown + inputfeld) 1 oder mehr
+    '''         - Wenn ausgewählt, Datei wählen und CSV ändern
+    '''         - CSV speichern
+    '''         - Pop Up zur Bestätigung
+    '''         
+    '''         TAB Logs
+    '''         - Logs Viewer
+    '''             - CSV öffnen
+    '''             - CSV überschreiben und speichern
+    '''             - Fehler
+    ''' </summary>
+    ''' 
+    Dim fh As FileHandler = New FileHandler()
+    Dim log As Logger = New Logger()
+    Dim originalFile As List(Of String()) = New List(Of String())
+
+    Private Sub CSVTool_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        log.Log("Info", "CSV Tool geöffnet")
+    End Sub
+
+    Private Sub btnImportCSV_Click(sender As Object, e As EventArgs) Handles btnImportCSV.Click
+        'Neue Liste erstellen
+        originalFile = New List(Of String())
+
+        'Inhalt auslesen und einzelne Daten speichern
+        Dim content As List(Of String) = fh.readFile()
+        If content IsNot "" Then
+            For i As Integer = 0 To content.Count - 1
+                originalFile.Add(content(i).Split(","))
+            Next
+            log.Log("Info", "CSV Datei erfolgreich geladen")
+        Else
+            log.Log("Error", "Fehler beim auslesen der Datei")
+        End If
+
+
+        'ListBox mit Titelzeile füllen
+        For i As Integer = 0 To originalFile(0).Count - 1
+            lbColumns.Items.Add(originalFile(0)(i))
+        Next
+    End Sub
+
+    Private Sub btnReplaceData_Click(sender As Object, e As EventArgs) Handles btnReplaceData.Click
+        If lbColumns.Items.Count = 0 Then
+            MsgBox("Bitte lade eine CSV Datei")
+        ElseIf lbColumns.SelectedItems.Count = 0 Then
+            MsgBox("Bitte wähle eine oder mehrere Spalten")
+        Else
+            'Inhalt auslesen und einzelne Daten speichern
+            Dim changesFile As List(Of String) = fh.readFile()
+            If changesFile IsNot "" Then
+                log.Log("Info", "Änderungsdatei erfolgreich geladen")
+            Else
+                log.Log("Error", "Fehler beim auslesen der Änderungsdatei")
+                Exit Sub
+            End If
+            For i As Integer = 0 To changesFile.Count - 1
+                Dim changes As String() = changesFile(i).Split(",")
+                Dim line As String() = originalFile(changes(0))
+                If line(lbColumns.SelectedIndex) = changes(1) Then
+                    line(lbColumns.SelectedIndex) = changes(2)
+                Else
+                    log.Log("Error", "Zeile " & changes(0) & " stimmt nicht überein, Änderungen überprüfen")
+                    MsgBox("Zeile " & changes(0) & " stimmt nicht überein, bitte Änderungen überprüfen")
+                    Exit Sub
+                End If
+            Next
+
+            'Neue Daten speichern und formatieren
+            Dim newFile As List(Of String) = New List(Of String)
+            For i As Integer = 0 To originalFile.Count - 1
+                newFile.Add(String.Join(",", originalFile(i)))
+            Next
+            fh.saveFile(newFile)
+            MsgBox("Erfolgreich geändert")
+            log.Log("Success", "Spalte " & lbColumns.SelectedItem & " wurde überschrieben")
+        End If
+    End Sub
+
+    Private Sub btnOpenLogs_Click(sender As Object, e As EventArgs) Handles btnOpenLogs.Click
+        MsgBox(log.readLogs())
+    End Sub
+End Class
