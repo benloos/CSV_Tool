83c77f195ab40d239a7e3c2f4bebdc3969a4113a
Ben Loos <ben.loos47@live.com>
2024-05-23 14:52:29 +0200

    add logger file
    write and read logs

---
+++ utils/Logger.vb
@@ -0,0 +1,16 @@
+﻿Imports System.IO
+
+Public Class Logger
+    Dim logFile As String = "csvtool_logs.txt"
+
+    'Logs auslesen
+    Public Function readLogs()
+        Return String.Join(Environment.NewLine, File.ReadAllLines(logFile))
+    End Function
+
+    'Log in die Datei schreiben
+    Public Sub Log(type As String, message As String)
+        Dim time As DateTime = DateTime.Now
+        File.AppendAllText(logFile, time & " | " & type & " | " & message & Environment.NewLine)
+    End Sub
+End Class
