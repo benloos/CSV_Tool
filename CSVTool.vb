1f4cd2726ce360cbb7fcb6a9d84d9052e02b8f59
Ben Loos <ben.loos47@live.com>
2024-05-27 10:11:22 +0200

    add log message when changes are shorter or longer than original

--- CSVTool.vb
+++ CSVTool.vb
@@ -2,27 +2,10 @@
 Imports System.Net
 
 Public Class CSVTool
-    ''' <summary>
-    ''' Struktur:
-    '''     GUI:
-    '''         TAB CSV
-    '''         - Datei laden mit csv
-    '''         - CSV Viewer
-    '''         - Spaltenauswahl (dropdown + inputfeld) 1 oder mehr
-    '''         - Wenn ausgewählt, Datei wählen und CSV ändern
-    '''         - CSV speichern
-    '''         - Pop Up zur Bestätigung
-    '''         
-    '''         TAB Logs
-    '''         - Logs Viewer
-    '''             - CSV öffnen
-    '''             - CSV überschreiben und speichern
-    '''             - Fehler
-    ''' </summary>
-    ''' 
-    Dim fh As FileHandler = New FileHandler()
-    Dim log As Logger = New Logger()
-    Dim originalFile As List(Of String()) = New List(Of String())
+
+    Dim fh As New FileHandler()
+    Dim log As New Logger()
+    Dim originalFile As New List(Of String())
 
     Private Sub CSVTool_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         lvCSV.View = View.Details
@@ -32,8 +15,8 @@ Public Class CSVTool
     End Sub
 
     Private Sub btnImportCSV_Click(sender As Object, e As EventArgs) Handles btnImportCSV.Click
-        'Neue Liste erstellen
-        originalFile = New List(Of String())
+        'Liste leeren
+        originalFile.Clear()
 
         'Inhalt auslesen und einzelne Daten speichern
         Dim content As List(Of String) = fh.readFile()
@@ -72,6 +55,12 @@ Public Class CSVTool
         ElseIf lbColumns.SelectedItems.Count = 0 Then
             MsgBox("Bitte wähle eine oder mehrere Spalten")
         Else
+            'Kopie zum modifizieren
+            Dim originalDeepcopy As New List(Of String())
+            For Each elem In originalFile
+                originalDeepcopy.Add(elem.Clone())
+            Next
+
             'Inhalt auslesen und einzelne Daten speichern
             Dim changesFile As List(Of String) = fh.readFile()
             If changesFile IsNot "" Then
@@ -81,37 +70,48 @@ Public Class CSVTool
                 Exit Sub
             End If
 
-            'Anzahl Kolonnen speichern
+            'Anzahl Kolonnen und Reihen speichern
             Dim columnCount = changesFile(0).Count(Function(x) x = ",") + 1
+            Dim rowCount = changesFile.Count
+            Dim origRowCount = originalDeepcopy.Count
+
+            'Vergleich Anzahl Reihen
+            If rowCount > origRowCount Then
+                log.Log("Error", "Zu viele Zeilen geladen, bitte Änderungsdatei überprüfen")
+                MsgBox("Zu viele Zeilen geladen, bitte Änderungen überprüfen")
+                Exit Sub
+            ElseIf rowCount < origRowCount Then
+                log.Log("Warning", "Nicht genügend Zeilen geladen, bitte Resultat überprüfen")
+            End If
 
             'Bei einzelner Kolonne wird alles überschrieben
             If columnCount = 1 Then
-                For i As Integer = 0 To changesFile.Count - 1
-                    originalFile(i + 1)(lbColumns.SelectedIndex) = changesFile(i)
+                For i As Integer = 0 To rowCount - 1
+                    originalDeepcopy(i + 1)(lbColumns.SelectedIndex) = changesFile(i)
                 Next
                 'Bei zwei Kolonnen werden alle Zeilen chronologisch überschrieben wenn sie übereinanderstimmen
             ElseIf columnCount = 2 Then
-                For i As Integer = 0 To changesFile.Count - 1
+                For i As Integer = 0 To rowCount - 1
                     Dim changes As String() = changesFile(i).Split(",")
-                    Dim line As String() = originalFile(i + 1)
+                    Dim line As String() = originalDeepcopy(i + 1)
                     If line(lbColumns.SelectedIndex) = changes(0) Then
                         line(lbColumns.SelectedIndex) = changes(1)
                     Else
-                        log.Log("Error", "Zeile " & i & " stimmt nicht überein, Änderungen überprüfen")
-                        MsgBox("Zeile " & i & " stimmt nicht überein, bitte Änderungen überprüfen")
+                        log.Log("Error", "Zeile " & i & " stimmt nicht überein, Änderungsdatei überprüfen")
+                        MsgBox("Zeile " & i & " stimmt nicht überein, bitte Änderungsdatei überprüfen")
                         Exit Sub
                     End If
                 Next
                 'Bei drei Kolonnen werden spezifische Zeilennummern überschrieben wenn sie übereinanderstimmen
             ElseIf columnCount = 3 Then
-                For i As Integer = 0 To changesFile.Count - 1
+                For i As Integer = 0 To rowCount - 1
                     Dim changes As String() = changesFile(i).Split(",")
-                    Dim line As String() = originalFile(changes(0))
+                    Dim line As String() = originalDeepcopy(changes(0))
                     If line(lbColumns.SelectedIndex) = changes(1) Then
                         line(lbColumns.SelectedIndex) = changes(2)
                     Else
-                        log.Log("Error", "Zeile " & changes(0) & " stimmt nicht überein, Änderungen überprüfen")
-                        MsgBox("Zeile " & changes(0) & " stimmt nicht überein, bitte Änderungen überprüfen")
+                        log.Log("Error", "Zeile " & changes(0) & " stimmt nicht überein, Änderungsdatei überprüfen")
+                        MsgBox("Zeile " & changes(0) & " stimmt nicht überein, bitte Änderungsdatei überprüfen")
                         Exit Sub
                     End If
                 Next
@@ -123,8 +123,8 @@ Public Class CSVTool
 
             'Neue Daten speichern und formatieren
             Dim newFile As List(Of String) = New List(Of String)
-            For i As Integer = 0 To originalFile.Count - 1
-                newFile.Add(String.Join(",", originalFile(i)))
+            For i As Integer = 0 To originalDeepcopy.Count - 1
+                newFile.Add(String.Join(",", originalDeepcopy(i)))
             Next
             Dim fileName As String = fh.saveFile(newFile)
             If fileName IsNot "" Then
